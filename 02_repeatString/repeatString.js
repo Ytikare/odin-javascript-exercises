ec589fdef9a3d52ddf8303c0c969a9e63aebd71a
Ytikare <ytikare2@tutanota.com>
2024-07-31 09:12:29 +0300

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatCount) {
+    if (repeatCount < 0) {
+        return 'ERROR';
+    }
+    return text.repeat(repeatCount);
 };
 
 // Do not edit below this line
