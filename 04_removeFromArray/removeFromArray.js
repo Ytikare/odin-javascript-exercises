9129e445d33a37614b3cb73ea7d6f3bcb85463a1
Ytikare <ytikare2@tutanota.com>
2024-08-03 13:41:08 +0300

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToRemove) {
+    for (let i = 0; i < elementToRemove.length; i++) {
+        let removeIndex = -1;
+
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === elementToRemove[i]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
