12295a61e5cb45ea92ea4f538e55b2bed1e88504
Ytikare <ytikare2@tutanota.com>
2024-08-13 10:38:54 +0300

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let text = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').replace(/\s{1,}/g,'');
 
+    for (let i = 0; i < text.length / 2; i++) {
+        if (text[i].toLowerCase() != text[text.length - 1 -i].toLowerCase()) {
+            return false
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
