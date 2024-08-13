b276ebd42fe50f2f2075ede490e3b1f18777c93a
Ytikare <ytikare2@tutanota.com>
2024-08-13 10:21:09 +0300

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, el) => total + el, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, el) => total * el, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let powerTotal = 1;
+  for (let i = 0; i < power; i++) {
+    powerTotal *= num;
+  }
+  return powerTotal;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
