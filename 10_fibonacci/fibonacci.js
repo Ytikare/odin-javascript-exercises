bcfa96e95a60628e62ec72c30905bc4ac8a79c92
Ytikare <ytikare2@tutanota.com>
2024-08-13 12:07:24 +0300

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (isNaN(number) || number < 0) {
+        return 'OOPS'
+    } else if (number == 0) {
+        return 0
+    } else if (number == 1 || number == 2) {
+        return 1;
+    } 
+    else {
+        let firstNum = 1;
+        let secondNum = 1;
 
+        for (let i = 2; i < number; i++) {
+            let temp = secondNum + firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+        }
+
+        return secondNum;
+    }
 };
 
 // Do not edit below this line
