bfdabe39bac118f786622b645853722d56665e22
Ytikare <ytikare2@tutanota.com>
2024-08-13 12:58:11 +0300

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
 
+        if (oldest.hasOwnProperty('name') == false) {
+            oldest['name'] = person.name;
+            oldest['yearOfBirth'] = person.yearOfBirth;
+            if (person.hasOwnProperty('yearOfDeath')) {
+                oldest['yearOfDeath'] = person['yearOfDeath'];
+            }
+        } else {
+
+            let currOldestPersonAge, secondAge; 
+
+
+            
+            if (oldest.hasOwnProperty('yearOfDeath')) {
+                currOldestPersonAge = getAgeForDeadPerson(oldest.yearOfBirth, oldest.yearOfDeath);
+            } else {
+                currOldestPersonAge = getAgeForAlivePerson(oldest.yearOfBirth);
+            }
+
+            if (person.hasOwnProperty('yearOfDeath')) {
+                secondAge = getAgeForDeadPerson(person.yearOfBirth, person.yearOfDeath);
+            } else {
+                secondAge = getAgeForAlivePerson(person.yearOfBirth);
+            }
+
+           oldest = currOldestPersonAge > secondAge ? oldest: person;
+        }
+
+        return oldest;
+    },{})
 };
+
+function getAgeForDeadPerson(yearOfBirth, yearOfDeath){
+    return yearOfDeath - yearOfBirth
+}
+
+function getAgeForAlivePerson(yearOfBirth){
+    return new Date().getFullYear() - yearOfBirth; 
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
